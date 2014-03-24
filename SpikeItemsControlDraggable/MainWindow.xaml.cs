9c5daeedea43efa35f42d44f2389375cda3ae879
Bernard Notarianni <bernard.notarianni@gmail.com>
2014-03-24 13:06:48 +0100

    creation solution

---
+++ SpikeItemsControlDraggable/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace SpikeItemsControlDraggable
+{
+
+    public class Task
+    {
+        public int Priority { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+    }
+    public class MyData : ObservableCollection<Task>
+    {
+        public MyData()
+        {
+            Add(new Task { Priority = 1, TaskName = "toto", Description = "un truc" });
+            Add(new Task { Priority = 2, TaskName = "titi", Description = "un truc" });
+            Add(new Task { Priority = 3, TaskName = "tutu", Description = "un truc" });
+        }
+    }
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
